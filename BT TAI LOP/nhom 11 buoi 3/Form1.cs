7d7d758aa8ef8f854ed7b1b7d3deadd988130983
Baothang <138952885+memaynhan@users.noreply.github.com>
2023-07-12 11:37:18 +0700

    Add files via upload

---
+++ BT TAI LOP/nhom 11 buoi 3/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace nhom_11_buoi_3
+{
+    public partial class Form1 : Form
+    {
+        Form2 form;
+        public Form1()
+        {
+            InitializeComponent();
+            form = new Form2(dataGridView1);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            form = new Form2(dataGridView1);
+            form.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            form = new Form2(dataGridView1);
+            form.Show();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var ask = MessageBox.Show("ban co muon xoa khong ?","exit",MessageBoxButtons.YesNo,MessageBoxIcon.Question);
+            if(ask ==DialogResult.Yes)
+            {
+                foreach(DataGridViewRow item in this.dataGridView1.SelectedRows)
+                {
+                    dataGridView1.Rows.RemoveAt(item.Index);    
+                }
+
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            var ask = MessageBox.Show("Dong giao dien ?", "Exit", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (ask == DialogResult.Yes)
+            {
+                this.Close();
+            }
+        }
+    }
+}
---
+++ BT TAI LOP/nhom 11 buoi 3/Form2.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace nhom_11_buoi_3
+{
+    public partial class Form2 : Form
+    {
+        DataGridView dataView = new DataGridView();
+        public Form2(DataGridView dataView)
+        {
+            InitializeComponent();
+            this.dataView = dataView;
+        }
+
+        private void InsertOrUpdate(int value)
+        {
+            dataView.Rows[value].Cells[0].Value = txtMANV.Text;
+            dataView.Rows[value].Cells[1].Value = txtTenNV.Text;
+            dataView.Rows[value].Cells[2].Value = txtLuongCB.Text;
+        }
+        private int findIdx(string id)
+        {
+            for(int i=0; i<dataView.Rows.Count; i++)
+            {
+                if ((string)(dataView.Rows[i].Cells[0].Value) == id)
+                {
+                    return i;
+                }
+               
+            }
+            return -1;
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                if (txtMANV.Text == "" || txtTenNV.Text == "" || txtLuongCB.Text == "")
+                {
+                    throw new Exception("Vui lòng nhập đầy đủ thông tin");
+                    return;
+                }
+                foreach (char i in txtTenNV.Text)
+                {
+                    for (char j = '0'; j <= '9'; j++)
+                    {
+                        if (i == j)
+                        {
+                            throw new Exception("Vui long khong nhap so !");
+                            return;
+                        }
+                    }
+                }
+                foreach (char i in txtLuongCB.Text)
+                {
+
+                    for (char j = 'a'; j <= 'z'; j++)
+                    {
+                        if (i == j) { throw new Exception("Vui long khong nhap chu !"); return; }
+                    }
+                }
+                int idx = findIdx(txtMANV.Text);
+                if (idx != -1)
+                {
+                    InsertOrUpdate(idx);
+                    MessageBox.Show("Cập nhật thành công");
+                }
+                else
+                {
+                    idx = dataView.Rows.Add();
+                    InsertOrUpdate(idx);
+                    MessageBox.Show("Thêm thành công");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
+
+
